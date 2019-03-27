9302b721b1946abb3b0ef5766aa7b4a5cafcaca3
JRJman <41746794+JRJman@users.noreply.github.com>
2019-03-27 11:11:35 +0100

    kan nu registeren

--- rojectoo/site/log_in_test/php/registeer.php
+++ rojectoo/site/log_in_test/php/registeer.php
@@ -1,3 +1,37 @@
 <?php
-  
+include "databse/data.php";
+  $gebruikersnaam = $_POST['gebruikersnaam2'];
+  $wachtwoord = $_POST['wachtwoord2'];
+  $herhaaldWachtwoord = $_POST['wachtwoord3'];
+
+  $con = mysqli_connect($servername,$uid,$pwd,$database);
+  if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+  }
+
+  if(checkWachtwoord($wachtwoord, $herhaaldWachtwoord) == true){
+    makeAccount($gebruikersnaam,$wachtwoord,$con);
+  }else{
+    header("Location: http://localhost/test/index.html?registeren=false");
+  }
+
+  function makeAccount($gebruikersnaam,$wachtwoord,$con){
+    $sql = "INSERT INTO account (id, gebruikersnaam, wachtwoord)
+    VALUES ('', '$gebruikersnaam', '$wachtwoord')";
+
+    if ($con->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+    }
+    header("Location: http://localhost/test/index.html");
+  }
+
+  function checkWachtwoord($wachtwoord, $herhaaldWachtwoord){
+    if($wachtwoord === $herhaaldWachtwoord){
+      return true;
+    }else{
+      return false;
+    }
+  }
 ?>
