ddfa24fd504d1bd0b32d7ce2181ed2839cb618d4
JRJman <41746794+JRJman@users.noreply.github.com>
2019-03-28 11:00:52 +0100

    new

--- rojectoo/site/log_in_test/php/database/databse.php
+++ rojectoo/site/log_in_test/php/database/databse.php
@@ -3,6 +3,7 @@
   $dt = "CREATE TABLE IF NOT EXISTS `accounts` (
           id              INT(6)        UNSIGNED AUTO_INCREMENT PRIMARY KEY,
           gebruikersnaam  VARCHAR(30)   NOT NULL,
-          wachtwoord      VARCHAR(30)   NOT NULL
+          wachtwoord      VARCHAR(30)   NOT NULL,
+          email           VARCHAR(60)   NOT NULL
     )"
 ?>
