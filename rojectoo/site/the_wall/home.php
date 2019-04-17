b72e2119d44087e0bdcc086a12009e4ee284c78b
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-17 16:08:20 +0200

    changes

--- rojectoo/site/the_wall/home.php
+++ rojectoo/site/the_wall/home.php
@@ -39,8 +39,8 @@ try {
     $vertificatie = $rij['vertificatie'];
   }
 
-  if($_SESSION == true){
-    if($_SESSION['vertificatie'] == $vertificatie){
+  if($_SESSION['session'] == "true"){
+    if($_SESSION['vertificatie'] != $vertificatie){
       header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/log_in.php");
     }
   } else {
