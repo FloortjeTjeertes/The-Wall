11247c558a3e8f0679a5b92a8fb926eb537ac4ba
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-17 16:00:43 +0200

    session changes

--- rojectoo/site/the_wall/home.php
+++ rojectoo/site/the_wall/home.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 
-if(empty($_SESSION)){
-  header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/log_in.php");
-} else if($_SESSION['session'] != "true") {
+if($_SESSION['session'] != "true") {
   header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/log_in.php");
 } else if(empty($_SESSION['id'])) {
   header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/log_in.php");
