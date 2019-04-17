dacbd9c05829f4e63fa453751db1f3f4d5ff75e1
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-17 17:48:09 +0200

    log in changes

--- rojectoo/site/the_wall/php/log_in.php
+++ rojectoo/site/the_wall/php/log_in.php
@@ -18,6 +18,8 @@
           $_SESSION['session'] = "true";
           $_SESSION['id'] = $rij['id'];
           $_SESSION['vertificatie'] = $rij['vertificatie'];
+          $_SESSION['active'] = "true";
+          header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/home.php");
         }
       }
     }
