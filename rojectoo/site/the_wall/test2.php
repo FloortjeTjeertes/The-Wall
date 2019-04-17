6e1be8bd2b3dd62629dcf3b23f14027e264e14a5
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-17 16:07:11 +0200

    tester

--- rojectoo/site/the_wall/test2.php
+++ rojectoo/site/the_wall/test2.php
@@ -1,9 +1,6 @@
 <?php
+  ini_set('display_errors', 1);
   session_start();
-  $_SESSION['session'] = "true";
-  $_SESSION['id'] = 2;
-  $_SESSION['vertificatie'] = "139KGP";
-  $_SESSION['active'] = "true";
   echo $_SESSION['id'] . "<br>";
   echo $_SESSION['vertificatie'] . "<br>";
   echo $_SESSION['active'] . "<br>";
