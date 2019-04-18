0f7916d33e5b2066229af1bd7047b877ef704512
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-18 12:08:34 +0200

    changes

--- rojectoo/site/the_wall/log_out.php
+++ rojectoo/site/the_wall/log_out.php
@@ -4,4 +4,5 @@
   $_SESSION['id'] = 0;
   $_SESSION['vertificatie'] = "0";
   $_SESSION['active'] = "false";
+  header("Location: http://26393.hosts2.ma-cloud.nl/bewijzenmap/periode1.3/proj/the_wall/log_in.php");
 ?>
