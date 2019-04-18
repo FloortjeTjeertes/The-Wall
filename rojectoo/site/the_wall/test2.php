5fca9f46a25bd7c3f7a40bd5a3d5673832992dd8
JRJman <41746794+JRJman@users.noreply.github.com>
2019-04-18 08:30:22 +0200

    testers weg

--- rojectoo/site/the_wall/test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-  ini_set('display_errors', 1);
-  session_start();
-  echo $_SESSION['id'] . "<br>";
-  echo $_SESSION['vertificatie'] . "<br>";
-  echo $_SESSION['active'] . "<br>";
-  echo $_SESSION['session'];
-?>
--- rojectoo/site/the_wall/test3.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION['session'] = "";
-$_SESSION['id'] = 0;
-$_SESSION['vertificatie'] = "";
-$_SESSION['active'] = "";
-?>
